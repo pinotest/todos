48c638b318c5300ab1bc7bf915dde795fa1fce50
Piotr Nowak <pinotestpiotrnowak@gmail.com>
2020-08-09 13:50:05 +0200

    Add new file to support sqllite database todos.db

---
+++ TodosSQLite.py
@@ -0,0 +1,91 @@
+import sqlite3
+from sqlite3 import Error
+
+
+class TodosSQLite:
+    def __init__(self, db_file="todos.db"):
+        self.db_file = db_file
+
+    def cursor(self):
+        self.conn = self.create_connection(self.db_file)
+        print("self.conn.cursor()", self.conn.cursor())
+        return self.conn.cursor()
+
+    def create_connection(self, db_file):
+        """ create a database connection to the SQLite database
+            specified by db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+            print("Successfully Connected to SQLite")
+            return conn
+        except Error as e:
+            print(e)
+        return conn
+
+    def get_all(self, table):
+        cur = self.cursor().execute(f"SELECT * FROM {table};")
+        print("cur", cur)
+        todos_list = cur.fetchall()
+        print("cur.fetchall()", cur.fetchall())
+        print("cur.execute", cur.execute("select id from todos;"))
+        print("cur.fetchall()", cur.fetchall())
+        cur.close()
+        return todos_list
+
+    def select_all(self):
+        """
+        Query all rows in the table
+        :param conn: the Connection object
+        :return:
+        """
+        cur = self.cursor()
+        cur.execute("SELECT * FROM todos")
+        rows = cur.fetchone()
+
+        return rows
+
+    def get(self, id):
+        #todo = [todo for todo in self.select_all() if todo['id'] == id]
+        todo = self.select_all()
+        print(todo)
+        if todo:
+            return todo[0]
+        return []
+
+    def add_todo(self, todo):
+        #     """
+        #     Add a new todo into the todos table
+        #     :param conn:
+        #     :param todo:
+        #     :return: todo id
+        #     """
+        sql = '''INSERT INTO todos(id, name, description, done)
+                VALUES(?,?,?,?)'''
+        cur = self.conn.cursor()
+        cur.execute(sql, todo)
+        self.conn.commit()
+        return cur.lastrowid
+
+    # def update(self, id, data):
+    #     todo = self.get(id)
+    #     if todo:
+    #         index = self.todos.index(todo)
+    #         self.todos[index] = data
+    #         return True
+    #     return False
+
+    # def delete(self, id):
+    #     todo = self.get(id)
+    #     if todo:
+    #         self.todos.remove(todo)
+    #         self.save_all()
+    #         return True
+    #     return False
+
+
+todos_SQL = TodosSQLite()
+# todos.create_connection("database.db")
